ef7ee7f74ff36ded843c7be1216d74b91cec8dfc
Bogdan <bogdanberezka@gmail.com>
2023-01-28 12:27:43 +0200

    delete-comments

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,15 +3,9 @@ import styles from './imageGalleryItem.module.css';
 const ImageGalleryItem = ({ id, src, bigImage }) => {
   return (
     <li className={styles.ImageGalleryItem} id={id}>
-      <img src={src} alt="image" className={styles.ImageGalleryItemImage} />
+      <img src={src} alt="bigImage" className={styles.ImageGalleryItemImage} />
     </li>
   );
 };
 
 export default ImageGalleryItem;
-
-{
-  /* <li class="gallery-item">
-  <img src="" alt="" />
-</li> */
-}
